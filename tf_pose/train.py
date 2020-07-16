5f0635542fa414e5032fd1a0713c04871706db00
chengdicao <chengdicao@foxmail.com>
2020-07-16 23:50:30 +0800

    fix test_var

--- tf_pose/train.py
+++ tf_pose/train.py
@@ -173,13 +173,15 @@ if __name__ == '__main__':
     config.gpu_options.allow_growth = True
     
     with tf.Session(config=config) as sess:
+        test_var = [var for var in tf.global_variables() if 'efficientnet-b0/stem/' in var.name ]
+        logger.info('backbone_var:')
+        logger.info(sess.run(test_var[0][0]))
+        test_var = [var for var in tf.global_variables() if 'Openpose' in var.name ]
+        logger.info('hat_var:')
+        logger.info(sess.run(test_var[0][0]))
+
         logger.info('model weights initialization')
         sess.run(tf.global_variables_initializer())
-
-        test_var = [var for var in tf.global_variables() if 'efficientnet-b0/stem/' in var.name ]
-        logger.info('backbone_var:%f' %sess.run(test_var[0][0]))
-        test_var = [var for var in tf.global_variables() if 'Openpose' in var.name ]
-        logger.info('hat_var:%f' %sess.run(test_var[0][0]))
 
         if args.checkpoint and os.path.isdir(args.checkpoint):
             logger.info('Restore from checkpoint...')
@@ -202,10 +204,12 @@ if __name__ == '__main__':
             logger.info('Restore pretrained weights...Done')
 
         test_var = [var for var in tf.global_variables() if 'efficientnet-b0/stem/' in var.name ]
-        logger.info('backbone_var:%f' %sess.run(test_var[0][0]))
+        logger.info('backbone_var:')
+        logger.info(sess.run(test_var[0][0]))
         test_var = [var for var in tf.global_variables() if 'Openpose' in var.name ]
-        logger.info('hat_var:%f' %sess.run(test_var[0][0]))
-
+        logger.info('hat_var:')
+        logger.info(sess.run(test_var[0][0]))
+        
         logger.info('prepare file writer')
         file_writer = tf.summary.FileWriter(os.path.join(logpath, args.tag), sess.graph)
 
@@ -225,9 +229,11 @@ if __name__ == '__main__':
             curr_epoch = float(gs_num) / step_per_epoch
 
             test_var = [var for var in tf.global_variables() if 'efficientnet-b0/stem/' in var.name ]
-            logger.info('backbone_var:%f' %sess.run(test_var[0][0]))
+            logger.info('backbone_var:')
+            logger.info(sess.run(test_var[0][0]))
             test_var = [var for var in tf.global_variables() if 'Openpose' in var.name ]
-            logger.info('hat_var:%f' %sess.run(test_var[0][0]))
+            logger.info('hat_var:')
+            logger.info(sess.run(test_var[0][0]))
 
             if gs_num > step_per_epoch * args.max_epoch:
                 break
