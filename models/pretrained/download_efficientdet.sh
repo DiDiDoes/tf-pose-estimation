8fb445378da38dc866a7bd7e1f3030dff2a9b984
chengdicao <chengdicao@foxmail.com>
2020-09-07 14:48:05 +0800

    efficientdet

--- models/pretrained/download_efficientdet.sh
+++ models/pretrained/download_efficientdet.sh
@@ -2,3 +2,6 @@ wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientdet/coco/effi
 tar -xzvf efficientdet-d0.tar.gz
 rm efficientdet-d0.tar.gz
 
+wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientdet/coco/efficientdet-d1.tar.gz
+tar -xzvf efficientdet-d1.tar.gz
+rm efficientdet-d1.tar.gz
